fbe37d9cf9b1d8bbbc0da753685449910ca7e584
Christine Reffo <christine.reffo@gmail.com>
2018-09-23 19:46:57 -0700

    Changed layout: colours, about me, experience, portfolio in double row

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-https://www.w3schools.com/howto/howto_js_typewriter.asp
 
 var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
+var txt = 'Lorem ipsum dummy text blabla.';
+
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementById("about_text").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }
+
+document.getElementById("animation_btn").addEventListener("click", typeWriter())
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
