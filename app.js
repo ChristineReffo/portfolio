451f8e9968cdd1017389ea4144c3885358859381
Christine Reffo <christine.reffo@gmail.com>
2018-09-27 20:08:24 -0700

    Added event: mouseover on project cards

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
 
-var i = 0;
-var txt = 'Strategy.' + <br /> + 'Front End.' + <br /> + 'Entrepreneurship.';
-
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementsByClassName("inner_tagline").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
-document.getElementById("animation_btn").addEventListener("click", typeWriter())
-
-
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
-
 
 
 // TODO:
@@ -29,4 +6,11 @@ $(function() {
 // - Have navbar hidden at first, show slowly or on scrolling
 // - Use typewriter animation for title --> how to get it into three lines
 // - resize portfolio images
-//- in page anchors for url references
+
+
+// Event listener to animate portfolio_wrap
+let projectCards = document.getElementById('gallery');
+
+projectCards.addEventListener('mouseover', function(event) {
+  event.target.classList.add('text_side')
+})
