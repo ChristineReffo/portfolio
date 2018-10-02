dfa2af437f3397e0ed3d7937ccc7facb938b62e2
Christine Reffo <christine.reffo@gmail.com>
2018-10-01 19:21:08 -0700

    Added animation: navbar, arrow fades on scroll

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 
 
+//Code adapted from W3 Schools, changed from sliding to fade animation: https://www.w3schools.com/howto/howto_js_navbar_slide.asp
+
+window.onscroll = function () {
+  scrollFunction();
+  arrowFade();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.opacity = "1";
+  } else {
+    document.getElementById("navbar").style.opacity = "0";
+  }
+};
+
+function arrowFade() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("arrow").style.opacity = "0";
+  }
+   else {
+    document.getElementById("arrow").style.opacity = "1";
+  }
+}
+
+
 
 // TODO:
 // - Fade out scroll down arrow when scrolling
@@ -9,8 +34,8 @@
 
 
 // Event listener to animate portfolio_wrap
-let projectCards = document.getElementById('gallery');
-
-projectCards.addEventListener('mouseover', function(event) {
-  event.target.classList.add('text_side')
-})
+// let projectCards = document.getElementById('gallery');
+//
+// projectCards.addEventListener('mouseover', function(event) {
+//   event.target.classList.add('text_side')
+// });
